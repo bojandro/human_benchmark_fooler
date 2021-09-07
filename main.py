fa62a61dc44cb444ee818201d1e6c4e44bdaf6b3
Vadym <cerber20134321@gmail.com>
2021-09-07 15:37:06 +0300

    add chimp test fooler

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ URLS = {
     'fool_reaction_time': r'https://humanbenchmark.com/tests/reactiontime',
     'fool_memorize_the_sequence': r'https://humanbenchmark.com/tests/sequence',
     'fool_aim_trainer': r'https://humanbenchmark.com/tests/aim',
-    'fool_number_memory': r'https://humanbenchmark.com/tests/number-memory'
+    'fool_number_memory': r'https://humanbenchmark.com/tests/number-memory',
+    'fool_chimp_test': r'https://humanbenchmark.com/tests/chimp'
 }
 
 
@@ -20,7 +21,7 @@ def check_and_create_dir(path: str):
     return
 
 
-def fooler_hof(func):
+def fooler_hof(func, goal: int = 10):
     def wrapper(*args, **kwargs):
         global URLS
 
@@ -33,7 +34,7 @@ def fooler_hof(func):
         driver.get(URLS[func_name])
 
         # Call a function
-        func(driver)
+        func(driver, goal)
 
         # Save screenshot
         path = f'results/{func_name}'
@@ -45,10 +46,11 @@ def fooler_hof(func):
     return wrapper
 
 
-def fool_reaction_time(driver: webdriver):
+def fool_reaction_time(driver: webdriver, goal):
+    number_of_shots = 5
     field = driver.find_element_by_class_name('view-splash')
 
-    for i in range(5):
+    for i in range(number_of_shots):
         field.click()
 
         while True:
@@ -57,14 +59,14 @@ def fool_reaction_time(driver: webdriver):
                 break
 
 
-def fool_memorize_the_sequence(driver: webdriver):
+def fool_memorize_the_sequence(driver: webdriver, goal):
     field = driver.find_element_by_class_name('css-de05nr')
     field.click()
 
     squares = driver.find_element_by_class_name('squares')
     sequence_global_len = 0
 
-    while True:
+    for _ in range(goal):
         sequence = []
         while sequence_global_len >= len(sequence):
             try:
@@ -85,18 +87,19 @@ def fool_memorize_the_sequence(driver: webdriver):
         sequence_global_len += 1
 
 
-def fool_aim_trainer(driver: webdriver):
+def fool_aim_trainer(driver: webdriver, goal):
+    number_of_shots = 30
     field = driver.find_element_by_css_selector('.css-1k4dpwl')
     field.click()
 
-    for i in range(30):
+    for i in range(number_of_shots):
         driver.find_element_by_xpath('/html/body/div[1]/div/div[4]/div[1]/div/div[1]/div/div/div/div[6]').click()
 
 
-def fool_number_memory(driver: webdriver):
+def fool_number_memory(driver: webdriver, goal):
     time_counter = 2.0
 
-    while True:
+    for _ in range(goal):
         start_button = driver.find_element_by_class_name('css-de05nr.e19owgy710')
         start_button.click()
 
@@ -111,9 +114,23 @@ def fool_number_memory(driver: webdriver):
         time_counter += 0.8
 
 
+def fool_chimp_test(driver, goal):
+    number_of_blocks = 4
+
+    for _ in range(goal - number_of_blocks):
+        start_button = driver.find_element_by_class_name('css-de05nr.e19owgy710')
+        start_button.click()
+
+        for i in range(number_of_blocks):
+            block = driver.find_element_by_css_selector(f'div[data-cellnumber="{i + 1}"]')
+            block.click()
+
+        number_of_blocks += 1
+
+
 if __name__ == '__main__':
-    # fooler_hof(fool_reaction_time)()
-    # fooler_hof(fool_aim_trainer)()
-    # fooler_hof(fool_memorize_the_sequence)()
-    # fooler_hof(fool_number_memory)()
-    pass
+    fooler_hof(fool_reaction_time, 7)()
+    fooler_hof(fool_aim_trainer, 7)()
+    fooler_hof(fool_memorize_the_sequence, 7)()
+    fooler_hof(fool_number_memory, 7)()
+    fooler_hof(fool_chimp_test, 7)()
