2b4546eb859c50ea3eff407d0ccf6f50372ef7e6
Bohdan <bogdan.loljoj@gmail.com>
2021-09-07 12:38:32 +0300

    generalize things with HOF

--- main.py
+++ main.py
@@ -1,54 +1,70 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time
+from datetime import datetime
+import os
 
 
-def fool_reaction_time():
-    url = r'https://humanbenchmark.com/tests/reactiontime'
+URLS = {
+    'fool_reaction_time': r'https://humanbenchmark.com/tests/reactiontime',
+    'fool_memorize_the_sequence': r'https://humanbenchmark.com/tests/sequence',
+    'fool_aim_trainer': r'https://humanbenchmark.com/tests/aim'
+}
 
-    driver = webdriver.Firefox()
-    driver.get(url)
 
-    time.sleep(1)
+def check_and_create_dir(path: str):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    return
 
+
+def fooler_hof(func):
+    def wrapper(*args, **kwargs):
+        global URLS
+
+        # Print in terminal
+        func_name = func.__name__
+        print(f'Starting {func_name}!')
+
+        # Initialize webdriver
+        driver = webdriver.Firefox()
+        driver.get(URLS[func_name])
+
+        # Call a function
+        func(driver)
+
+        # Save screenshot
+        path = f'results/{func_name}'
+        check_and_create_dir(path)
+        driver.save_screenshot(os.path.join(path, f'result-{datetime.now()}.png'))
+
+        # Close tab
+        driver.close()
+    return wrapper
+
+
+def fool_reaction_time(driver: webdriver):
     field = driver.find_element_by_class_name('view-splash')
 
-    field.click()
+    for i in range(5):
+        field.click()
 
-    while True:
-        try:
-            driver.find_element_by_class_name('view-waiting')
-        except Exception:
-            field.click()
-            break
-
-    driver.save_screenshot(f'results/reaction_time/result-{time.time()}.png')
-
-    time.sleep(3)
-
-    driver.close()
+        while True:
+            if 'view-go' in field.get_attribute('class'):
+                field.click()
+                break
 
 
-def fool_memorize_the_sequence():
-    url = r'https://humanbenchmark.com/tests/sequence'
-
-    driver = webdriver.Firefox()
-    driver.get(url)
-    print('Connection established!')
-
-    time.sleep(1)
-
-    print('Starting benchmark!')
+def fool_memorize_the_sequence(driver: webdriver):
     field = driver.find_element_by_class_name('css-de05nr')
     field.click()
 
     squares = driver.find_element_by_class_name('squares')
-
-    sequence_global = []
+    sequence_global_len = 0
 
     while True:
         sequence = []
-        while len(sequence_global) >= len(sequence):
+        while sequence_global_len >= len(sequence):
             try:
                 curr_square = squares.find_element_by_class_name('active')
                 sequence.append(curr_square)
@@ -64,29 +80,23 @@ def fool_memorize_the_sequence():
         for el in sequence:
             el.click()
 
-        sequence_global.append(sequence[-1])
+        sequence_global_len += 1
 
 
-def fool_aim_trainer():
-    url = r'https://humanbenchmark.com/tests/aim'
-
-    driver = webdriver.Firefox()
-    driver.get(url)
-
-    time.sleep(1)
-
-    print('Started shooting')
+def fool_aim_trainer(driver: webdriver):
     field = driver.find_element_by_css_selector('.css-1k4dpwl')
-    print(field)
     field.click()
 
     for i in range(30):
-        field = driver.find_element_by_css_selector('div.css-17nnhwz:nth-child(6)')
-        print(field)
-        field.click()
+        driver.find_element_by_xpath('/html/body/div[1]/div/div[4]/div[1]/div/div[1]/div/div/div/div[6]').click()
+
+    driver.save_screenshot(f'results/aim_trainer/result-{datetime.now()}.png')
+
+    driver.close()
 
 
 if __name__ == '__main__':
-    # fool_reaction_time()
-    fool_memorize_the_sequence()
-    # fool_aim_trainer()
+    # fooler_hof(fool_reaction_time)()
+    # fooler_hof(fool_memorize_the_sequence)()
+    # fooler_hof(fool_aim_trainer)()
+    pass
